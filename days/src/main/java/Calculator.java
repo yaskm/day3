3ef6ce4034e7714268b3a77cdb99c55ef8c471cc
Yashwanth K M <yaskm@publicisgroupe.net>
2021-04-28 09:27:01 +0530

    sub feature

--- days/src/main/java/Calculator.java
+++ days/src/main/java/Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator{
-   public int add(int n1 ,int n2){
+   public static int add(int n1 ,int n2){
      return n1+n2;
    }
+  public static int sub(int n1 ,int n2){
+	return n1-n2;
+  }
+ 
 
 }
