c37e4fa8fdf319ffa8f1894798aad0809ba191b1
Yashwanth K M <yaskm@publicisgroupe.net>
2021-04-28 09:32:43 +0530

    mul feature

--- days/src/main/java/Calculator.java
+++ days/src/main/java/Calculator.java
@@ -1,10 +1,12 @@
 public class Calculator{
    public static int add(int n1 ,int n2){
-     return n1+n2;
+      return n1+n2;
    }
   public static int sub(int n1 ,int n2){
 	return n1-n2;
   }
- 
+  public static int mul(int n1 ,int n2){
+       return n1*n2;
+  }
 
 }
